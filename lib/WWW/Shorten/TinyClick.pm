076eae810588b9a01b508196a4b142cab7d3c2b4
Dave Cross <dave@dave.org.uk>
2007-04-15 15:19:34 +0000

    tinyclick.com is dead

--- lib/WWW/Shorten/TinyClick.pm
+++ lib/WWW/Shorten/TinyClick.pm
@@ -1,4 +1,15 @@
 # $Id$
+package WWW::Shorten::TinyClick;
+
+use strict;
+use warnings;
+
+our $VERSION = '1.90';
+require WWW::Shorten::_dead;
+
+0;
+
+__END__
 
 =head1 NAME
 
@@ -6,98 +17,15 @@ WWW::Shorten::TinyClick - Perl interface to tinyclick.com
 
 =head1 SYNOPSIS
 
-  use WWW::Shorten::TinyClick;
-
-  use WWW::Shorten 'TinyClick';
-
-  $short_url = makeashorterlink($long_url);
-
-  $long_url  = makealongerlink($short_url);
+    # No appropriate synopsis.
 
 =head1 DESCRIPTION
 
-A Perl interface to the web site tinyclick.com.  TinyClick simply
-maintains a database of long URLs, each of which has a unique
-identifier.
+A Perl interface to the web site tinyclick.com.
 
-=cut
-
-package WWW::Shorten::TinyClick;
-
-use 5.006;
-use strict;
-use warnings;
-
-use base qw( WWW::Shorten::generic Exporter );
-our @EXPORT = qw(makeashorterlink makealongerlink);
-our $VERSION = sprintf "%d.%02d", '$Revision$ ' =~ /(\d+)\.(\d+)/;
-
-use Carp;
-use URI;
-
-=head1 Functions
-
-=head2 makeashorterlink
-
-The function C<makeashorterlink> will call the TinyClick web site
-passing it your long URL and will return the shorter TinyClick version.
-
-=cut
-
-sub makeashorterlink ($)
-{
-    my $url = shift or croak 'No URL passed to makeashorterlink';
-    my $ua = __PACKAGE__->ua();
-    my $uri = URI->new('http://tinyclick.com/MakeLinkSmaller.cfm');
-
-    $uri->query_form(
-	LongLink => $url,
-    );
-    my $resp  = $ua->get( $uri );
-    return unless $resp->is_success;
-    if ($resp->content =~ m!
-	(\Qhttp://tinyclick.com/?\E\w+)
-	!x) {
-	return $1;
-    }
-    return;
-}
-
-=head2 makealongerlink
-
-The function C<makealongerlink> does the reverse. C<makealongerlink>
-will accept as an argument either the full TinyClick URL or just the
-TinyClick identifier.
-
-If anything goes wrong, then either function will return C<undef>.
-
-=cut
-
-sub makealongerlink ($)
-{
-    my $tiny_url = shift 
-	or croak 'No TinyClick key / URL passed to makealongerlink';
-    my $ua = __PACKAGE__->ua();
-
-    $tiny_url =~ s{ ^\Qhttp://tinyclick.com/\E \?? }{}x;
-    $tiny_url = "http://tinyclick.com/?$tiny_url"
-    unless $tiny_url =~ m!^http://!i;
-
-    my $resp = $ua->get($tiny_url);
-    return undef unless $resp->is_redirect;
-    my $url = $resp->header('Location');
-    return $1 if $url =~ m[(http://.*$)];
-    return;
-
-}
-
-1;
-
-__END__
-
-=head2 EXPORT
-
-makeashorterlink, makealongerlink
+Unfortunately, this service became inactive at some point between 1.89
+and 1.90, so this module will merely give you an error if you try to use
+it. Feel free to pick a different L<service|WWW::Shorten>.
 
 =head1 SUPPORT, LICENCE, THANKS and SUCH
 
@@ -105,10 +33,10 @@ See the main L<WWW::Shorten> docs.
 
 =head1 AUTHOR
 
-Iain Truskett <spoon@cpan.org>
+Iain Truskett <spoon@cpan.org>, Dave Cross <dave@mag-sol.com>
 
 =head1 SEE ALSO
 
-L<WWW::Shorten>, L<perl>, L<http://tinyclick.com/>
+L<WWW::Shorten>, L<perl>
 
 =cut
--- t/tinyclick.t
+++ t/tinyclick.t
@@ -1,18 +1,3 @@
-use Test::More tests => 7;
-
-BEGIN { use_ok WWW::Shorten::TinyClick };
-
-my $url = 'http://books.perl.org/category/13';
-my $short = makeashorterlink( $url );
-ok((defined $short), 'Reasonable response');
-like ( $short => qr{http://tinyclick\.com/\??..}, 'make it shorter' );
-
-is ( makealongerlink( $short ) => $url, 'make it longer' );
-
-my ($rs) = $short =~ /\? (.*) $/x;
-is ( makealongerlink($rs) => $url, 'make it longer by Id' );
-
-eval { &makeashorterlink() };
-ok($@);
-eval { &makealongerlink() };
-ok($@);
+use Test::More tests => 1;
+eval { require WWW::Shorten::TinyClick };
+like( $@,  qr/inactive/, "Service correctly reports it is inactive." );
